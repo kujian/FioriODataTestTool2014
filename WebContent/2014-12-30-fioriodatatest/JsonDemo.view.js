f9e16f0991c017dee25e7c774005585588e5e3ba
i042416 <jerry.wang@sap.com>
2014-12-31 17:15:46 +0800

    now drop down list in table can work as table column, and consume built-in model of table control. No dedicated JSON model for drop down list is created any more

--- WebContent/2014-12-30-fioriodatatest/JsonDemo.view.js
+++ WebContent/2014-12-30-fioriodatatest/JsonDemo.view.js
@@ -51,28 +51,15 @@ sap.ui.jsview("2014-12-30-fioriodatatest.JsonDemo", {
         }));
         
      // fourth column "Options"
-        
-        var oDropdownModel = new sap.ui.model.json.JSONModel();
-        oDropdownModel.setData({
-        	hardware:[
-        		{device:"PC", enabled:true},
-        		{device:"Monitor", enabled:true},
-        		{device:"Keyboard", enabled:false},
-        		{device:"Mouse", enabled:true},
-        		{device:"Speaker", enabled:false},
-        		{device:"Printer", enabled:true}],
-        	editable: true, 
-        	tooltip: "Device"});
 
         // Create a DropdownBox
         var oDropdownBox3 = new sap.ui.commons.DropdownBox("DropdownBox3");
-        oDropdownBox3.bindProperty("tooltip", "/tooltip");
-        oDropdownBox3.bindProperty("editable", "/editable");
-        oDropdownBox3.setModel(oDropdownModel);
+        //oDropdownBox3.bindProperty("tooltip", "/tooltip");
+        //oDropdownBox3.bindProperty("editable", "/editable");
         var oItemTemplate1 = new sap.ui.core.ListItem();
         oItemTemplate1.bindProperty("text", "device");
         oItemTemplate1.bindProperty("enabled", "enabled");
-        oDropdownBox3.bindItems("/hardware", oItemTemplate1);
+        oDropdownBox3.bindItems("hardware", oItemTemplate1);
 
         oTable.addColumn(new sap.ui.table.Column({
                label : new sap.ui.commons.Label({
@@ -82,7 +69,36 @@ sap.ui.jsview("2014-12-30-fioriodatatest.JsonDemo", {
                width : "100px"
         }));
         
-        
+        // another drop down list <<<<<<<< 5 >>>>>>>>>>>>>
+        /*
+        var oDropdownBox4 = new sap.ui.commons.DropdownBox("DropdownBox4");
+        var oDropdownModel4 = new sap.ui.model.json.JSONModel();
+        oDropdownModel4.setData({
+        	hardware:[
+        		{device:"PC", enabled:true},
+        		{device:"Monitor", enabled:true},
+        		{device:"Keyboard", enabled:false},
+        		{device:"Mouse", enabled:true},
+        		{device:"Speaker", enabled:false},
+        		{device:"Printer", enabled:true}],
+        	editable: true, 
+        	tooltip: "Device"});
+        oDropdownBox4.bindProperty("tooltip", "/tooltip");
+        oDropdownBox4.bindProperty("editable", "/editable");
+        oDropdownBox4.setModel(oDropdownModel4);
+        var oItemTemplate2 = new sap.ui.core.ListItem();
+        oItemTemplate2.bindProperty("text", "device");
+        oItemTemplate2.bindProperty("enabled", "enabled");
+        oDropdownBox4.bindItems("/hardware", oItemTemplate2);
+
+        oTable.addColumn(new sap.ui.table.Column({
+               label : new sap.ui.commons.Label({
+                     text : "Options2"
+               }),
+               template : oDropdownBox4,
+               width : "100px"
+        }));
+        */
         return oTable;
 	},
 	
@@ -106,7 +122,7 @@ sap.ui.jsview("2014-12-30-fioriodatatest.JsonDemo", {
          
          
          // handle with text Area
-         oInput = new sap.ui.commons.TextArea('input1');
+         /*oInput = new sap.ui.commons.TextArea('input1');
          oInput.bindProperty("value","jsontext>/Description");
          oInput.setTooltip("This is a tooltip");
          oInput.setRows(5);
@@ -115,9 +131,10 @@ sap.ui.jsview("2014-12-30-fioriodatatest.JsonDemo", {
          oInput.setModel(textModel, "jsontext");
          
          oInput.attachChange(function(){alert('Text changed to :'+ oInput.getValue());});
-         this._oInput = oInput;
+         this._oInput = oInput;*/
          var oLayout = new sap.ui.layout.VerticalLayout("Layout1", {
-         	content: [oTable, oInput]
+         	//content: [oTable, oInput]
+        	 content:[oTable]
          });
          oLayout.placeAt("content");
 	}
