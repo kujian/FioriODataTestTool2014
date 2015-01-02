b79a02ee413ace868ce849a3f0f2abbb3de9df0b
i042416 <jerry.wang@sap.com>
2015-01-02 11:45:44 +0800

    now countdown start will use tomorrow 00:00:00AM as reference

--- WebContent/countdown/misc.js
+++ WebContent/countdown/misc.js
@@ -48,3 +48,24 @@ function display_message(msg, type) {
 function hide_error() {
 	$('#error_message').fadeOut('slow');
 }
+
+function getDeadline() {
+	var myDate = new Date();
+	var time = myDate.getTime();
+	var inteval = 1 * 1000 * 3600 * 24;
+	time += inteval;
+	var newDate = new Date(time);
+	console.log("full year: " + newDate.getFullYear());   
+	console.log("month: " + newDate.getMonth());     
+	console.log("date: " + newDate.getDate());  
+	return {
+		year: newDate.getFullYear(),
+		month: parseInt(newDate.getMonth(), 10 ) + 1,
+		date: newDate.getDate()
+	};
+}
+	
+function getDeadlineString(deadline) {
+	return deadline.year + "/" + deadline.month + "/" + deadline.date + " 00:00:00AM";
+}
+
