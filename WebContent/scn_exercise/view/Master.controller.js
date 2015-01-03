498214b5c5f7444556ad0eafc85a0a1ca718e0eb
i042416 <jerry.wang@sap.com>
2015-01-03 12:48:03 +0800

    search bar works now

--- WebContent/scn_exercise/view/Master.controller.js
+++ WebContent/scn_exercise/view/Master.controller.js
@@ -5,5 +5,21 @@ sap.ui.controller("scn_exercise.view.Master", {
 	handleListItemPress : function (evt) {
 		var context = evt.getSource().getBindingContext();
 		this.nav.to("Detail", context);
+	},
+	
+	handleSearch : function (evt) {
+		
+		// create model filter
+		var filters = [];
+		var query = evt.getParameter("query");
+		if (query && query.length > 0) {
+			var filter = new sap.ui.model.Filter("SoId", sap.ui.model.FilterOperator.Contains, query);
+			filters.push(filter);
+		}
+		
+		// update list binding
+		var list = this.getView().byId("list");
+		var binding = list.getBinding("items");
+		binding.filter(filters);
 	}
 });
