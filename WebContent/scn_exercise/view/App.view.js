ee3e00a6ff639f89ebe9cf82862ebee1cf3c47ae
i042416 <jerry.wang@sap.com>
2015-01-03 14:23:42 +0800

    splitApp works

--- WebContent/scn_exercise/view/App.view.js
+++ WebContent/scn_exercise/view/App.view.js
@@ -9,16 +9,16 @@ sap.ui.jsview("scn_exercise.view.App", {
 		// to avoid scroll bars on desktop the root view must be set to block display
 		this.setDisplayBlock(true);
 		
-		this.app = new sap.m.App();
+		this.app = new sap.m.SplitApp();
 		
 		// load the master page
 		var master = sap.ui.xmlview("Master", "scn_exercise.view.Master");
 		master.getController().nav = this.getController();
 		this.app.addPage(master, true);
 		
-		var detail = sap.ui.xmlview("Detail", "scn_exercise.view.Detail");
-		detail.getController().nav = this.getController();
-		this.app.addPage(detail, false);
+		// load the empty page
+		var empty = sap.ui.xmlview("Empty", "scn_exercise.view.Empty");
+		this.app.addPage(empty, false);
 		
 		return this.app;
 	}
