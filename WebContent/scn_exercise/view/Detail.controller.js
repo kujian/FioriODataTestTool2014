5e3fbae9578266404b70d3f30ed45076275fcefe
i042416 <jerry.wang@sap.com>
2015-01-03 16:23:32 +0800

    exercise 8 approve button ok

--- WebContent/scn_exercise/view/Detail.controller.js
+++ WebContent/scn_exercise/view/Detail.controller.js
@@ -1,4 +1,6 @@
 jQuery.sap.require("scn_exercise.util.Formatter");
+jQuery.sap.require("sap.m.MessageBox"); 
+jQuery.sap.require("sap.m.MessageToast");
 
 sap.ui.controller("scn_exercise.view.Detail", {
 
@@ -7,5 +9,24 @@ sap.ui.controller("scn_exercise.view.Detail", {
 	},
 	onBeforeRendering:function(){ 
 		this.byId("SupplierForm").bindElement("BusinessPartner"); 
+	},
+	
+	handleApprove : function (evt) {
+
+		// show confirmation dialog
+		var bundle = this.getView().getModel("i18n").getResourceBundle();
+		sap.m.MessageBox.confirm(
+			bundle.getText("ApproveDialogMsg"),
+			function (oAction) {
+				if (sap.m.MessageBox.Action.OK === oAction) {
+					// notify user
+					var successMsg = bundle.getText("ApproveDialogSuccessMsg");
+					sap.m.MessageToast.show(successMsg);
+					// TODO call proper service method and update model (not part of this session)
+				}
+			},
+			
+			bundle.getText("ApproveDialogTitle")
+		);
 	}
 });
