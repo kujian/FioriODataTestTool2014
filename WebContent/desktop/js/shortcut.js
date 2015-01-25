0bcc58df0f3a304fffed39d8f64d109277e7f1cc
i042416 <jerry.wang@sap.com>
2015-01-25 09:57:16 +0800

    jQuery desktop

---
+++ WebContent/desktop/js/shortcut.js
@@ -0,0 +1,14 @@
+//id,iconName,iconUrl,url,width,height
+var shortcut = [
+[0,"我的电脑","desktop/images/icos/01.png","http://www.w2bc.com/?",1000,500],
+[1,"我的图片","desktop/images/icos/02.png","http://www.w2bc.com/",1000,500],
+[2,"我的音乐","desktop/images/icos/03.png","http://www.w2bc.com/",1000,500],
+[3,"我的指南","desktop/images/icos/04.png","http://www.w2bc.com/",800,500],
+[4,"应用程序","desktop/images/icos/05.png","http://www.w2bc.com/",600,500],
+[5,"我的私信","desktop/images/icos/06.png","http://www.w2bc.com/",1000,500],
+[6,"色盘","desktop/images/icos/07.png","http://www.w2bc.com/",1000,500],
+[7,"云上传","desktop/images/icos/08.png","http://www.w2bc.com/",1000,500],
+[8,"云下载","desktop/images/icos/09.png","http://www.w2bc.com/",1000,500],
+[9,"行程","desktop/images/icos/10.png","http://www.w2bc.com/",1000,500],
+[10,"记事本","desktop/images/icos/11.png","http://www.w2bc.com/",900,540],
+];
